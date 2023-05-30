83051e637e65c2ca7e03a2bb3df128b5f56b3d25
CT Pranjal <pranjal@clevertap.com>
2023-05-30 14:22:49 +0530

    js folder

---
+++ js/clevertap_sw.js
@@ -0,0 +1,3 @@
+// this is a service worker file https://developer.clevertap.com/docs/web-push#add-the-service-worker-file
+//this is for push as chrome uses a special script-service worker which can listen web pushes.
+importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js');
